be4c94fe393469f6971c4dcfb29affb219ae993f
Shinobu Okano <okanoshinobu8@gmail.com>
2017-06-30 21:48:39 +0900

    ⚡

--- src/main/java/gson/Main.java
+++ src/main/java/gson/Main.java
@@ -23,5 +23,58 @@ public class Main {
 
         Animal animal = new Cat();
         System.out.println(gson.toJson(animal)); // {"name":"cat"}
+
+        // interfaceの型の場合、実装の型に合わせてjson化される
+        // プリミティブは初期値があるので、初期値でjson化される
+        System.out.println(gson.toJson(new Muga(new TestImpl(true, "test", true)))); // {"test":{"a":true,"b":"test","c":true}}
+        System.out.println(gson.toJson(new Muga(new TestImpl2(true)))); // {"test":{"a":true}}
+        System.out.println(gson.toJson(new Muga(new TestImpl()))); // {"test":{"a":false}}
+    }
+
+    public interface Test {
+        boolean getA();
+    }
+
+    public static class Muga {
+        public Test test;
+
+        public Muga(Test test) {
+            this.test = test;
+        }
+    }
+
+    public static class TestImpl implements Test {
+
+        public boolean a;
+        public String b;
+        public Boolean c;
+
+        public TestImpl() {
+        }
+
+        public TestImpl(boolean a, String b, boolean c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        @Override
+        public boolean getA() {
+            return a;
+        }
+    }
+
+    public static class TestImpl2 implements Test {
+
+        public boolean a;
+
+        public TestImpl2(boolean a) {
+            this.a = a;
+        }
+
+        @Override
+        public boolean getA() {
+            return a;
+        }
     }
 }
